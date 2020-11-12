59e187dbe155ea2de3828d1762de6f2458d3b783
rcclay <j.k.rofling@gmail.com>
2020-11-12 12:15:28 -0700

    Add empty check_h executable

---
+++ src/Drivers/check_h.cpp
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source
+// License.  See LICENSE file in top directory for details.
+//
+// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+//
+// File developed by:
+// Jeongnim Kim, jeongnim.kim@intel.com,
+//    Intel Corp.
+// Amrita Mathuriya, amrita.mathuriya@intel.com,
+//    Intel Corp.
+//
+// File created by:
+// Jeongnim Kim, jeongnim.kim@intel.com,
+//    Intel Corp.
+////////////////////////////////////////////////////////////////////////////////
+// -*- C++ -*-
+/** @file check_spo.cpp
+ * @brief Miniapp to check 3D spline implementation against the reference.
+ */
+
+int main(int argc, char** argv)
+{
+  return 0;
+}
